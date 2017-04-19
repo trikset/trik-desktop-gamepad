a540fca1d4483c130339a9f475c58d6f84b75c24
kbatoev <kbatoev@mail.ru>
2017-04-19 15:55:21 +0300

    Added slot to divide business logic and events

--- accelerateStrategy.cpp
+++ accelerateStrategy.cpp
@@ -1,122 +1,195 @@
 #include "accelerateStrategy.h"
 
 #include <QDebug>
+#include <math.h>
 
 AccelerateStrategy::AccelerateStrategy()
-	: powerX1(0)
-	, powerY1(0)
-	, powerX2(0)
-	, powerY2(0)
-	, timer(new QTimer(this))
-	, pad1WasActive(false)
+	: pad1WasActive(false)
 	, pad2WasActive(false)
+	, speed(300)
 {
-	connect(timer, SIGNAL(timeout()), this, SLOT(stopPads()));
+	connect(&stopTimer, SIGNAL(timeout()), this, SLOT(stopPads()));
+	connect(&workTimer, SIGNAL(timeout()), this, SLOT(dealWithPads()));
+
+	pad1 = {Qt::Key_W, Qt::Key_A, Qt::Key_S, Qt::Key_D};
+	pad2 = {Qt::Key_Left, Qt::Key_Right, Qt::Key_Up, Qt::Key_Down};
+	magicButtons = {Qt::Key_1, Qt::Key_2, Qt::Key_3, Qt::Key_4, Qt::Key_5};
+
+
+	powers = {
+		{X1, 0}
+		, {Y1, 0}
+		, {X2, 0}
+		, {Y2, 0}
+	};
+
+	cntPowers = {
+		{X1, 0}
+		, {Y1, 0}
+		, {X2, 0}
+		, {Y2, 0}
+	};
+
+	indeces = {
+		{Qt::Key_D, X1}
+		, {Qt::Key_A, X1}
+		, {Qt::Key_W, Y1}
+		, {Qt::Key_S, Y1}
+		, {Qt::Key_Right, X2}
+		, {Qt::Key_Left, X2}
+		, {Qt::Key_Up, Y2}
+		, {Qt::Key_Down, Y2}
+	};
 	additions = {
-		{Qt::Key_W, 10}
-		, {Qt::Key_S, -10}
-		, {Qt::Key_D, 10}
+		{Qt::Key_D, 10}
 		, {Qt::Key_A, -10}
+		, {Qt::Key_W, 10}
+		, {Qt::Key_S, -10}
+		, {Qt::Key_Right, 10}
+		, {Qt::Key_Left, -10}
 		, {Qt::Key_Up, 10}
 		, {Qt::Key_Down, -10}
-		, {Qt::Key_Left, -10}
-		, {Qt::Key_Right, 10}
 	};
+
+}
+
+AccelerateStrategy::AccelerateStrategy(int currentSpeed)
+	: AccelerateStrategy()
+{
+	speed = currentSpeed;
 }
 
 void AccelerateStrategy::processEvent(QEvent *event)
 {
 	int eventType = event->type();
 	if (eventType == QEvent::KeyPress || eventType == QEvent::KeyRelease) {
-		timer->start(500);
-	}
-	QSet<int> pad1 = {Qt::Key_W, Qt::Key_A, Qt::Key_S, Qt::Key_D};
-	QSet<int> pad2 = {Qt::Key_Left, Qt::Key_Right, Qt::Key_Up, Qt::Key_Down};
-	QSet<int> digit = {Qt::Key_1, Qt::Key_2, Qt::Key_3, Qt::Key_4, Qt::Key_5};
+		QKeyEvent *keyEvent = dynamic_cast<QKeyEvent *> (event);
+		auto key = static_cast<Qt::Key> (keyEvent->key());
+		QSet<int> allKeys = pad1 + pad2 + magicButtons;
+		if (allKeys.contains(key)) {
+			if (!workTimer.isActive())
+				workTimer.start(speed);
 
-	if (eventType == QEvent::KeyPress) {
-		auto key = (dynamic_cast<QKeyEvent *> (event))->key();
-
-		if (pad1.contains(key)) {
-			mPressedKeys += key;
-			pad1WasActive = true;
-			if (powerX1 && !mPressedKeys.contains(Qt::Key_D) && !mPressedKeys.contains(Qt::Key_A))
-				powerX1 = 0;
-			if (powerY1 && !mPressedKeys.contains(Qt::Key_W) && !mPressedKeys.contains(Qt::Key_S))
-				powerY1 = 0;
-			for (auto pressedKey : mPressedKeys) {
-				switch (pressedKey) {
-				case Qt::Key_W:
-					powerY1 = powerY1 < 100 ? powerY1 + 10 : 100;
-					break;
-				case Qt::Key_S:
-					powerY1 = powerY1 > -100 ? powerY1 - 10 : -100;
-					break;
-				case Qt::Key_D:
-					powerX1 = powerX1 < 100 ? powerX1 + 10 : 100;
-					break;
-				case Qt::Key_A:
-					powerX1 = powerX1 > -100 ? powerX1 - 10 : -100;
-					break;
+			if (magicButtons.contains(key))
+				dealWithButtons(keyEvent);
+			else {
+				if (!keyEvent->isAutoRepeat()) {
+					if (eventType == QEvent::KeyPress)
+						mPressedKeys += key;
+					else
+						mPressedKeys -= key;
 				}
 			}
-			// maybe timer2 for sending information
-			QString command = QString("pad 1 %1 %2 \n").arg(powerX1).arg(powerY1);
-			emit commandPrepared(command);
 		}
 
-		if (pad2.contains(key)) {
-			mPressedKeys += key;
-			pad2WasActive = true;
-			if (powerX2 && !mPressedKeys.contains(Qt::Key_Right) && !mPressedKeys.contains(Qt::Key_Left))
-				powerX2 = 0;
-			if (powerY2 && !mPressedKeys.contains(Qt::Key_Up) && !mPressedKeys.contains(Qt::Key_Down))
-				powerY2 = 0;
-			for (auto pressedKey : mPressedKeys) {
-				switch (pressedKey) {
-				case Qt::Key_Up:
-					powerY2 = powerY2 < 100 ? powerY2 + 10 : 100;
-					break;
-				case Qt::Key_Down:
-					powerY2 = powerY2 > -100 ? powerY2 - 10 : -100;
-					break;
-				case Qt::Key_Right:
-					powerX2 = powerX2 < 100 ? powerX2 + 10 : 100;
-					break;
-				case Qt::Key_Left:
-					powerX2 = powerX2 > -100 ? powerX2 - 10 : -100;
-					break;
-				}
-			}
-			// maybe timer2 for sending information
-			QString command = QString("pad 2 %1 %2 \n").arg(powerX2).arg(powerY2);
-			emit commandPrepared(command);
-		}
-
-
-
-	} else if (eventType == QEvent::KeyRelease) {
-		//int flag = 1;
-		auto key = (dynamic_cast<QKeyEvent *> (event))->key();
-		mPressedKeys -= key;
 	}
+}
+
+void AccelerateStrategy::setSpeed(int newSpeed)
+{
+	speed = newSpeed;
 }
 
 void AccelerateStrategy::stopPads()
 {
-	timer->stop();
-	const QString padup = "pad %1 up\n";
+	workTimer.stop();
+	stopTimer.stop();
+	const QString padUp = "pad %1 up\n";
 	if (pad1WasActive) {
-		powerX1 = 0;
-		powerY1 = 0;
+		powers[X1] = powers[Y1] = 0;
+		cntPowers[X1] = cntPowers[X1] = 0;
 		pad1WasActive = false;
-		emit commandPrepared(padup.arg(1));
+		emit commandPrepared(padUp.arg(1));
 	}
 	if (pad2WasActive) {
-		powerX2 = 0;
-		powerY2 = 0;
+		powers[X2] = powers[Y2] = 0;
+		cntPowers[X2] = cntPowers[Y2] = 0;
 		pad2WasActive = false;
-		emit commandPrepared(padup.arg(2));
+		emit commandPrepared(padUp.arg(2));
+	}
+}
+
+void AccelerateStrategy::dealWithPads()
+{
+	if (mPressedKeys.size()) {
+		stopTimer.start(2 * speed + 100);
+
+		// for pad1
+		bool isSomeKeyFromPad1 = false;
+		for (auto pad1Key : pad1) {
+			if (mPressedKeys.contains(pad1Key)) {
+				isSomeKeyFromPad1 = true;
+				pad1WasActive = true;
+				Power index = indeces[pad1Key];
+				powers[index] = std::max(-100, std::min(100, powers[index] + additions[pad1Key]));
+				cntPowers[index] = 0;
+			}
+		}
+		if (pad1WasActive) {
+			checkPower(powers[X1], cntPowers[X1], QSet<Qt::Key> {Qt::Key_A, Qt::Key_D});
+			checkPower(powers[Y1], cntPowers[Y1], QSet<Qt::Key> {Qt::Key_W, Qt::Key_S});
+		}
+
+		if (isSomeKeyFromPad1) {
+			QString command = QString("pad 1 %1 %2 \n").arg(powers[X1]).arg(powers[Y1]);
+			emit commandPrepared(command);
+		}
+
+		// for pad2
+		bool isSomeKeyFromPad2 = false;
+		for (auto pad2Key : pad2) {
+			if (mPressedKeys.contains(pad2Key)) {
+				isSomeKeyFromPad2 = true;
+				pad2WasActive = true;
+				Power index = indeces[pad2Key];
+				powers[index] = std::max(-100, std::min(100, powers[index] + additions[pad2Key]));
+				cntPowers[index] = 0;
+			}
+		}
+		if (pad2WasActive) {
+			checkPower(powers[X2], cntPowers[X2], QSet<Qt::Key> {Qt::Key_Left, Qt::Key_Right});
+			checkPower(powers[Y2], cntPowers[Y2], QSet<Qt::Key> {Qt::Key_Up, Qt::Key_Down});
+		}
+
+		if (isSomeKeyFromPad2) {
+			QString command = QString("pad 2 %1 %2 \n").arg(powers[X2]).arg(powers[Y2]);
+			emit commandPrepared(command);
+		}
+	}
+}
+
+void AccelerateStrategy::dealWithButtons(QKeyEvent *keyEvent)
+{
+	QMap<int, int> digits = {
+		{Qt::Key_1, 1}
+		, {Qt::Key_2, 2}
+		, {Qt::Key_3, 3}
+		, {Qt::Key_4, 4}
+		, {Qt::Key_5, 5}
+	};
+
+	auto key = keyEvent->key();
+	if (keyEvent->type() == QEvent::KeyPress) {
+		QString command = QString("btn " + QString::number(digits[key]) + "\n");
+		emit commandPrepared(command);
+	}
+}
+
+void AccelerateStrategy::checkPower(int &power, int &cnt, QSet<Qt::Key> set)
+{
+	if (power) {
+		bool contains = false;
+		for (auto key : set) {
+			contains = contains || mPressedKeys.contains(key);
+		}
+		if (!contains) {
+			if (cnt) {
+				cnt = 0;
+				power = 0;
+			} else {
+				cnt++;
+			}
+		}
 	}
 }
 
--- accelerateStrategy.h
+++ accelerateStrategy.h
@@ -9,22 +9,53 @@ class AccelerateStrategy : public Strategy
 
 public:
 	AccelerateStrategy();
+	AccelerateStrategy(int speed);
 
+	/// slot for getting events from UI
 	void processEvent(QEvent *event);
 
+	void setSpeed(int newSpeed);
+
 private slots:
+	/// slot for stopping pads if they were active
 	void stopPads();
 
-private:
-	int powerX1;
-	int powerY1;
-	int powerX2;
-	int powerY2;
+	/// slot that analyzes mPressedKeys and generates pad-commands
+	/// pads values are changed iteratively every period of time
+	void dealWithPads();
 
-	QTimer *timer;
+	/// slot for Magic Buttons
+	void dealWithButtons(QKeyEvent *keyEvent);
+
+private:
+
+	enum Power {
+		X1 = 0
+		, Y1
+		, X2
+		, Y2
+	};
+
+	/// checks if some key from set was pressed no longer than 1 tact
+	void checkPower(int &power, int &cnt, QSet<Qt::Key> set);
+	QMap<Power, int> powers;
+	QMap<int, Power> indeces;
+	QMap<int, int> additions;
+
+	/// variables for setting 0 to PowerVariables if they were not pressed more than 1 tact
+	QMap<Power, int> cntPowers;
+
+	QTimer stopTimer;
 	bool pad1WasActive;
 	bool pad2WasActive;
 	QSet<int> mPressedKeys;
-	QMap<int, int> additions;
+
+	QSet<int> pad1;
+	QSet<int> pad2;
+	QSet<int> magicButtons;
+	QTimer workTimer;
+
+	/// defines period of time to check dealWithPads
+	int speed;
 };
 
